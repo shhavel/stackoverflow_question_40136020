fcb734076ba0e965aff89807262fc1f9355339e8
Oleksandr Avoyants <shhavel@gmail.com>
2016-10-19 21:38:49 +0300

    Update factories test to match example condition

--- spec/factories_spec.rb
+++ spec/factories_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 describe 'Factories' do
   it 'creates 20 users, each with at least 10 posts, each post with at least 1 video, each video with at least 1 comment' do
-    FactoryGirl.create_list(:user, 20, :with_10_posts)
+    create_list(:user, 20, :with_10_posts)
     expect(User.count).to eq(20)
     user = User.take
-    expect(user.posts.count).to eq(10)
+    expect(user.posts.count).to be >= 10
     post = user.posts.take
-    expect(post.videos.count).to eq(1)
+    expect(post.videos.count).to be >= 1
     video = post.videos.take
-    expect(video.comments.count).to eq(1)
+    expect(video.comments.count).to be >= 1
   end
 end
